9331fea8ccd5cca8b18ed63256f23404a7bb4015
harry-kaur <harnoorkaur16165@gmail.com>
2025-10-11 18:44:38 +0530

    Updated list

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -2,9 +2,14 @@
 
 function List(){
 
-    const fruits = ["apple", "orange", "banana", "coconut", "pineapple"];
+    const fruits = [{name:"apple",calories:95},
+                    {name:"orange",calories:90},
+                    {name:"banana",calories:105},
+                    {name:"coconut",calories:159},
+                    {name:"mango",calories:95},];
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp;
+                                <b>{fruit.calories}</b></li>);
 
     return(<ul>{listItems}</ul>);
 }
