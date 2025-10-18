02cd4f8a7c4ba74a261e3e98472b20b5519b916c
harry-kaur <harnoorkaur16165@gmail.com>
2025-10-18 18:52:33 +0530

    update Age jsx

--- my-react-app/src/Age.jsx
+++ my-react-app/src/Age.jsx
@@ -1,7 +1,34 @@
 import React, { useState } from "react";
 
 function Age(){
+  const[name,setName]= useState("Guest");
+  const[age,setAge]= useState(0);
+  const[isEmployed,setIsEmployed]= useState(false);
 
-}
+  const updateName= ()=> {
+    setName("Kunwar");
+  }
+
+  const incrementAge = () => {
+    setAge(age +1);
+  }
+  const toggleEmployedStatus = () => {
+  setIsEmployed(!isEmployed);
+  }
+
+
+  return(
+
+     <div>
+        <p>Name: {name}</p>
+        <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick={incrementAge}>Increment Name</button>
+
+        <p>Is employed: {isEmployed ? "Yes": "No"}</p>
+        <button onClick={toggleEmployedStatus}>Toggle Status</button>
+     </div>);}
+
 
 export default Age
